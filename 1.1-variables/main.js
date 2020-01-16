948b8563dd77b378146438ba1d1dcaeea6428e70
Vakabunga <shesterov@gmail.com>
2020-01-16 20:22:21 +0300

    Update main.js

---
+++ 1.1-variables/main.js
@@ -0,0 +1,24 @@
+//Задача №1
+console.log('Задача №1');
+
+let algebra, geography, physics;
+algebra = 5;
+geography = 4;
+physics = 4;
+let avarageMark = (algebra + geography + physics) / 3;
+console.log(`Средняя оценка ${avarageMark}`);
+
+//Задача №2
+console.log('Задача №2');
+
+let myName = 'Всевластий';
+console.log(`Привет, мир! Меня зовут ${myName}`)
+
+//Задача №3
+console.log('Задача №3');
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
