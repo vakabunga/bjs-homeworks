da57feeb2752c22121b31330a4e09bac4703ca57
Vakabunga <shesterov@gmail.com>
2020-01-28 15:06:51 +0300

    correct some Math and optimize code

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,4 +1,4 @@
-
+"use strict";
 
 function calculateQuadraticEquation() {
     let a = +window.a.value;
@@ -11,15 +11,13 @@ function calculateQuadraticEquation() {
 }
 
 function getResult(a, b, c) {
-    "use strict";
-    debugger;
     let d = b ** 2 - 4 * a * c;
     let x = [];
     if (d === 0)
-        x[0] = b / 2 * a;
+        x[0] = -b / (2 * a);
     else if (d > 0) {
-        x[0] = (-b + Math.sqrt(d)) / 2 * a;
-        x[1] = (-b - Math.sqrt(d)) / 2 * a;
+        x[0] = (-b + Math.sqrt(d)) / (2 * a);
+        x[1] = (-b - Math.sqrt(d)) / (2 * a);
     }
     return x;
 }
@@ -36,11 +34,11 @@ function getAverageMark(marks) {
         marks = marks.slice(0, 5);
     }
     let sum = 0;
-    for (i = 0; i < marks.length; i++) {
+    for (let i = 0; i < marks.length; i++) {
         sum += marks[i];
     }
-    averageMark = sum / marks.length;
-    return averageMark;
+
+    return (sum / marks.length);
 }
 
 function calculateDrinkTask() {
@@ -52,14 +50,12 @@ function calculateDrinkTask() {
 
 function askDrink(name, dateOfBirthday) {
     let result;
-    let today = new Date();
-    let age = today.getFullYear() - dateOfBirthday.getFullYear();
 
-    if (age > 18)
+    if ((new Date().getFullYear() - dateOfBirthday.getFullYear()) > 18)
         result = `Не желаете ли олд-фэшн, ${name}?`;
     else
         result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
-        
+
     console.log(result);
     return result;
 }
