afc0f0cd778be120b69321d8afcc340b0e4546c6
Vakabunga <shesterov@gmail.com>
2020-02-03 23:16:56 +0300

    all tasks done

--- 2.1-wrappers-for-primitives/task.js
+++ 2.1-wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,9 +12,25 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    if (Number(percent) || percent == 0)
+        percent = Number(percent) / 100;
+    else return (`Параметр "Процентная ставка" содержит неправильное значение ${percent}`);
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    if (Number(contribution) || contribution == 0)
+        contribution = Number(contribution);
+    else return (`Параметр "Начальный взнос" содержит неправильное значение ${contribution}`);
+
+    if (Number(amount) || amount == 0)
+        amount = Number(amount);
+    else return (`Параметр "Общая стоимость" содержит неправильное значение ${amount}`);
+
+    if (date.indexOf('-') > 4)
+        return (`Введите правильно год в дате!!!!!`);
+
+    let loan = amount - contribution;
+    let amountOfMonths = (Number(date.substring(0, 4)) - new Date().getFullYear()) * 12 + Number(date.substring(5, 7)) - new Date().getMonth() - 1;
+    let monthlyPayment = loan * ((percent / 12) + (percent / 12) / (Math.pow((1 + percent / 12), amountOfMonths) - 1));
+    return ((monthlyPayment * amountOfMonths).toFixed(2));
 }
 
 function sayHello() {
@@ -23,6 +41,7 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    if (name === 'null' || name === 'undefined' || name === '' || isFinite(name))
+        return (`Привет, мир! Меня зовут Аноним`);
+    else return (`Привет, мир! Меня зовут ${name}`);
 }
