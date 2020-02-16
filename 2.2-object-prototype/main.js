9bddb9916e8f91018f537ec45f78faedd1540796
Vakabunga <shesterov@gmail.com>
2020-02-16 11:58:32 +0300

    third task done

--- 2.2-object-prototype/main.js
+++ 2.2-object-prototype/main.js
@@ -1,13 +1,32 @@
 function initCheckBirthday() {
     const birthday = document.getElementById('birthday').value;
-
+    console.log(birthday);
     const result = checkBirthday(birthday) ? "Да" : "Нет";
 
     document.getElementById('disclaimer').innerHTML = result;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
+    let now = new Date(); //текущая дата
+    let dateOfBirth = new Date(Number(birthday.substring(0, 4)), (Number(birthday.substring(5, 7)) - 1), Number(birthday.substring(8, 10))); //конвертация входящей даты
+    let diff = +now - +dateOfBirth; // разница в милисекундах между текущей датой и др
+    let leapCounter = 0; //Счетчик високосных годов
+    let counter = 0; // счетчик невисокосных годов
+    for (let i = dateOfBirth.getFullYear(); i < now.getFullYear(); i++) { // вычисляем сколько високосных и невисокосных годов было со дня рождения
+        if (checkLeapYear(i)) leapCounter++;
+        else counter++;
+    }
+    let checkYearOfBirth = new Date(dateOfBirth.getFullYear(), 01, 28); //задаем дату 28 фераля для года рождения
+
+    // Если человек родился в високосный год до 28 февраля, то високосность этого года не влияет на вычисление возраста
+    if ((+checkYearOfBirth > +dateOfBirth) && (checkLeapYear(dateOfBirth.getFullYear()))) leapCounter--;
+
+    let age = diff / ((leapCounter * 31623200000 + counter * 31536000000) / (leapCounter + counter));
+    return age > 18;
+
+    function checkLeapYear(year) { // функция определения високосного года
+        return((!(year % 400)) || ((year % 100) && !(year % 4)));
+    }
 }
 
 function initPrintAnimalSound() {
