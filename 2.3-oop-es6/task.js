82e1d0106a8242dd1f821905324db6fb1073ebfa
Vakabunga <shesterov@gmail.com>
2020-03-03 00:21:30 +0300

    1st and 2nd tasks has fixed

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -1,11 +1,11 @@
 //Задача №1
 // class Weapon {
-//     constructor(name, attack, durability, range) {
-//         this.name = name;
-//         this.attack = attack;
-//         this.durability = durability;
-//         this.baseDurability = durability;
-//         this.range = range;
+//     constructor(features) {
+//         this.name = features.name;
+//         this.attack = features.attack;
+//         this.durability = features.durability;
+//         this.baseDurability = features.durability;
+//         this.range = features.range;
 //     }
 //     takeDamage(damage) {
 //         if (this.durability > damage) this.durability -= damage;
@@ -32,13 +32,6 @@
 
 //Задача №2
 class Weapon {
-    constructor(name, attack, durability, range) {
-        this.name = name;
-        this.attack = attack;
-        this.durability = durability;
-        this.baseDurability = durability;
-        this.range = range;
-    }
     takeDamage(damage) {
         if (this.durability > damage) this.durability -= damage;
         else if (this.durability === Infinity) this.durability = Infinity;
@@ -55,98 +48,101 @@ class Weapon {
 }
 
 class Arm extends Weapon {
-    constructor(name, attack, durability, range) {
-        super(name, attack, durability, range)
+    constructor() {
+        super()
+        this.name = 'Рука';
+        this.attack = 1;
+        this.durability = Infinity;
+        this.baseDurability = this.durability;
+        this.range = 1;
         this.type = 'Рука';
     }
 }
 
 class Bow extends Weapon {
-    constructor(name, attack, durability, range) {
-        super(name, attack, durability, range)
+    constructor() {
+        super()
+        this.name = 'Лук';
+        this.attack = 10;
+        this.durability = 200;
+        this.baseDurability = this.durability;
+        this.range = 3;
         this.type = 'Лук';
     }
 }
 
 class Sword extends Weapon {
-    constructor(name, attack, durability, range) {
-        super(name, attack, durability, range)
+    constructor() {
+        super()
+        this.name = 'Меч';
+        this.attack = 25;
+        this.durability = 500;
+        this.baseDurability = this.durability;
+        this.range = 1;
         this.type = 'Меч';
     }
 }
 
 class Knife extends Weapon {
-    constructor(name, attack, durability, range) {
-        super(name, attack, durability, range)
+    constructor() {
+        super()
+        this.name = 'Нож';
+        this.attack = 5;
+        this.durability = 300;
+        this.baseDurability = this.durability;
+        this.range = 1;
         this.type = 'Нож';
     }
 }
 
 class Staff extends Weapon {
-    constructor(name, attack, durability, range) {
-        super(name, attack, durability, range)
+    constructor() {
+        super()
+        this.name = 'Посох';
+        this.attack = 8;
+        this.durability = 300;
+        this.baseDurability = this.durability;
+        this.range = 2;
         this.type = 'Посох';
     }
 }
 
-class ReinforcedArm extends Arm {
-    constructor(name, attack, durability, range) {
-        super(name)
-        attack === '-' ? this.attack = arm.attack : this.attack = attack;
-        durability === '-' ? this.durability = arm.durability : this.durability = durability;
-        range === '-' ? this.range = arm.range : this.range = range;
-        this.baseDurability = this.durability;
-    }
-}
-
 class ReinforcedBow extends Bow {
-    constructor(name, attack, durability, range) {
-        super(name)
-        attack === '-' ? this.attack = bow.attack : this.attack = attack;
-        durability === '-' ? this.durability = bow.durability : this.durability = durability;
-        range === '-' ? this.range = bow.range : this.range = range;
-        this.baseDurability = this.durability;
+    constructor() {
+        super()
+        this.name = 'Длинный лук';
+        this.attack = 15;
+        this.range = 4;
     }
 }
 
 class ReinforcedSword extends Sword {
-    constructor(name, attack, durability, range) {
-        super(name)
-        attack === '-' ? this.attack = sword.attack : this.attack = attack;
-        durability === '-' ? this.durability = sword.durability : this.durability = durability;
-        range === '-' ? this.range = sword.range : this.range = range;
-        this.baseDurability = this.durability;
-    }
-}
-
-class ReinforcedKnife extends Knife {
-    constructor(name, attack, durability, range) {
-        super(name)
-        attack === '-' ? this.attack = knife.attack : this.attack = attack;
-        durability === '-' ? this.durability = knife.durability : this.durability = durability;
-        range === '-' ? this.range = knife.range : this.range = range;
+    constructor() {
+        super()
+        this.name = 'Секира';
+        this.attack = 27;
+        this.durability = 800;
         this.baseDurability = this.durability;
     }
 }
 
 class ReinforcedStaff extends Staff {
-    constructor(name, attack, durability, range) {
-        super(name)
-        attack === '-' ? this.attack = staff.attack : this.attack = attack;
-        durability === '-' ? this.durability = staff.durability : this.durability = durability;
-        range === '-' ? this.range = staff.range : this.range = range;
-        this.baseDurability = this.durability;
+    constructor() {
+        super()
+        this.name = 'Посох Бури';
+        this.attack = 10;
+        this.range = 3;
     }
 }
 
-const arm = new Arm('Рука', 1, Infinity, 1);
-const bow = new Bow('Лук', 10, 200, 3);
-const sword = new Sword('Меч', 25, 500, 1);
-const knife = new Knife('Нож', 5, 300, 1);
-const staff = new Staff('Посох', 8, 300, 2);
-const longBow = new ReinforcedBow('Длинный лук', 15, '-', 4);
-const axe = new ReinforcedSword('Секира', 27, 800, '-');
-const stormStaff = new ReinforcedStaff('Посох Бури', 10, '-', 3);
+const arm = new Arm();
+const bow = new Bow();
+const sword = new Sword();
+const knife = new Knife();
+const staff = new Staff();
+const longBow = new ReinforcedBow();
+const axe = new ReinforcedSword();
+const stormStaff = new ReinforcedStaff();
 
 //Задача №3
 
