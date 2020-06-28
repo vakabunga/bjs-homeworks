bf0260cc43aec1128333c1e6d8f7ea681b972a75
Vakabunga <shesterov@gmail.com>
2020-06-28 23:05:56 +0300

    add testCase()

--- 3.1-array-proto-hocs/main.js
+++ 3.1-array-proto-hocs/main.js
@@ -4,7 +4,6 @@ function sleep(milliseconds) {
 }
 
 function sum(...args) {
-  // Замедление на половину секунды.
   sleep(500);
   return args.reduce((sum, arg) => {
     return (sum += +arg);
@@ -21,16 +20,37 @@ function memorize(fn, limit) {
     const result = memory.find((item) => {
       return compareArrays(item.args, Array.from(args));
     });
-    console.log(result);
+    // console.log(result);
     if (result) {
-      console.log('Результат взят из памяти');
+      // console.log('Результат взят из памяти');
       return result.result;
     } else {
       const newResult = fn(...args);
       memory.push({ args: Array.from(args), result: newResult });
       if (memory.length > limit) memory.shift();
-      console.log('Это новый результат');
+      // console.log('Это новый результат');
       return newResult;
     }
   };
 }
+
+function testCase(testFunction, timer) {
+  const testArray = [
+    [1, 2, 3],
+    [1, 2],
+    [1, 2, 3],
+    [1, 2],
+    [9, 5, 2, 4],
+  ];
+  console.time(timer);
+  for (let i = 0; i < 100; i++) {
+    testArray.forEach((elem) => testFunction(...elem));
+  }
+  console.timeEnd(timer);
+}
+
+const mSum = memorize(sum, 5);
+
+testCase(sum, 'timer of sum');
+
+testCase(mSum, 'timer of mSum');
